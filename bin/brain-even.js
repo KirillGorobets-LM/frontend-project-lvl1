29c4639499f490871e2e2b5065dbdf4fac80b7d4
kirill gorobets <kirya@live-master.com>
2020-11-29 09:21:14 +0300

    update

--- bin/brain-even.js
+++ bin/brain-even.js
@@ -1,33 +1,8 @@
 #!/usr/bin/env node
-import readlineSync from 'readline-sync';
+import { gameEngine } from '../src/index.js';
+import { rules, question, correctAnswer } from '../games/even-game.js';
+import { greeting } from '../src/cli.js';
 
-console.log('Welcome to the Brain Games!');
-const nameOfUser = readlineSync.question('May I have your name?');
-console.log(`Hello, ${nameOfUser}!`);
-console.log('Answer \'yes\' if the number is even, otherwise answer \'no\'.');
-
-const getRundom = () => Math.floor(Math.random() * (100 - 10)) + 10;
-const isEven = (num) => {
-  if (num % 2 === 0) {
-    return true;
-  }
-  return false;
-};
-const getCorrectAnswer = (num) => {
-  if (isEven(num)) {
-    return 'yes';
-  } return 'no';
-};
-const array = [getRundom(), getRundom(), getRundom()];
-for (let i = 0; i < array.length; i += 1) {
-  console.log(`Question: ${array[i]}`);
-  const answer = readlineSync.question('Your answer:');
-  if (getCorrectAnswer(array[i]) === answer) {
-    console.log('Correct!');
-  } else {
-    console.log(`'${answer}' is wrong answer ;(. Correct answer was '${getCorrectAnswer(array[i])}'`);
-    console.log(`Let\`s try again ${nameOfUser}!`);
-    process.exit();
-  }
-}
-console.log(`Congratulations, ${nameOfUser}!`);
+const nameOfUser = greeting();
+rules();
+gameEngine(question, correctAnswer, nameOfUser);
