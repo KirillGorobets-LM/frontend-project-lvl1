eabf5857a1d13fb1b212082adf08ce023d04feb3
kirill gorobets <kirya@live-master.com>
2020-12-01 14:28:01 +0300

    update even-game

--- bin/brain-even.js
+++ bin/brain-even.js
@@ -1,8 +1,4 @@
 #!/usr/bin/env node
-import { gameEngine } from '../src/index.js';
-import { rules, question, correctAnswer } from '../games/even-game.js';
-import { greeting } from '../src/cli.js';
+import evenGame from '../games/even-game.js';
 
-const nameOfUser = greeting();
-rules();
-gameEngine(question, correctAnswer, nameOfUser);
+evenGame();
--- games/even-game.js
+++ games/even-game.js
@@ -1,8 +1,16 @@
-export const rules = () => console.log('Answer \'yes\' if the number is even, otherwise answer \'no\'.');
-export const question = () => Math.floor(Math.random() * (100 - 10)) + 10;
-export const correctAnswer = (num) => {
-  if (num % 2 === 0) {
-    return 'yes';
-  } return 'no';
+import { greeting } from '../src/cli.js';
+import { gameEngine } from '../src/index.js';
+
+const evenGame = () => {
+  const userName = greeting();
+  console.log('Answer \'yes\' if the number is even, otherwise answer \'no\'.');
+  const getQuestionAndCorrectAnswer = () => {
+    const question = Math.floor(Math.random() * (100 - 10)) + 10;
+    console.log(`Question: ${question}`);
+    if (question % 2 === 0) {
+      return 'yes';
+    } return 'no';
+  };
+  gameEngine(getQuestionAndCorrectAnswer, userName);
 };
-export default rules;
+export default evenGame;
