15fd788c33c7484afbcb81c1368b8cf04478b2f1
kirill gorobets <kirya@live-master.com>
2020-12-02 15:29:02 +0300

    update gameEngine

--- src/index.js
+++ src/index.js
@@ -3,12 +3,12 @@ import readlineSync from 'readline-sync';
 const gameEngine = (getQuestionAndCorrectAnswer, userName) => {
   const countOfRounds = 3;
   for (let i = 0; i < countOfRounds; i += 1) {
-    const ca = getQuestionAndCorrectAnswer();
+    const correctAnswer = getQuestionAndCorrectAnswer();
     const answer = readlineSync.question('Your answer:');
-    if (ca === answer) {
+    if (correctAnswer === answer) {
       console.log('Correct!');
     } else {
-      console.log(`'${answer}' is wrong answer ;(. Correct answer was '${ca}'`);
+      console.log(`'${answer}' is wrong answer ;(. Correct answer was '${correctAnswer}'`);
       console.log(`Let's try again, ${userName}!`);
       process.exit();
     }
