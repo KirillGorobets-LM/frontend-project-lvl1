0cc5343fde7533483fe67c4d998652b594997dd9
kirill gorobets <kirya@live-master.com>
2020-12-03 15:18:55 +0300

    update gcd game

--- games/gcd-game.js
+++ games/gcd-game.js
@@ -10,14 +10,20 @@ const gcdGame = () => {
     const b = getRandomNumFromRange(10, 100);
     const question = `${a} ${b}`;
     let correctAnswer = '';
-    const bigNum = Math.max(a, b);
-    for (let i = bigNum; i >= 0; i -= 1) {
-      if (a % i === 0 && b % i === 0) {
-        correctAnswer = i.toString();
-        break;
+    const findMaxCommonDivisor = (num1, num2) => {
+      let result = 0;
+      const bigNum = Math.max(num1, num2);
+      for (let i = bigNum; i >= 0; i -= 1) {
+        if (a % i === 0 && b % i === 0) {
+          result = i;
+          break;
+        }
       }
-    }
+      return result.toString();
+    };
+    correctAnswer = findMaxCommonDivisor(a, b);
     const game = { question, correctAnswer };
+    console.log(game);
     return game;
   };
   gameEngine(getGameObject, userName);
