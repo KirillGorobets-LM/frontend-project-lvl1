a76455201bb12867c2c933bf72bde9ba3d307669
kirill gorobets <kirya@live-master.com>
2020-12-14 19:40:18 +0300

    update

--- src/games/calc-game.js
+++ src/games/calc-game.js
@@ -7,13 +7,17 @@ const operators = ['+', '-', '*'];
 const getResultOfExpression = (num1, num2, operator) => {
   let result = 0;
   switch (operator) {
-    case '+': result = num1 + num2;
+    case '+':
+      result = num1 + num2;
       break;
-    case '-': result = num1 - num2;
+    case '-':
+      result = num1 - num2;
       break;
-    case '*': result = num1 * num2;
+    case '*':
+      result = num1 * num2;
       break;
     default:
+      result = undefined;
   }
   return result;
 };
