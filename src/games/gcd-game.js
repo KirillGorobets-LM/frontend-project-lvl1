5a2af15615b22d6d27377ff18f0f03297a5426b6
kirill gorobets <kirya@live-master.com>
2020-12-14 19:28:35 +0300

    update

--- src/games/gcd-game.js
+++ src/games/gcd-game.js
@@ -6,7 +6,7 @@ const task = 'Find the greatest common divisor of given numbers.';
 const findMaxCommonDivisor = (num1, num2) => {
   let result = 0;
   const minNum = Math.max(num1, num2);
-  for (let i = minNum; i >= 0; i -= 1) {
+  for (let i = minNum; i >= 1; i -= 1) {
     if (num1 % i === 0 && num2 % i === 0) {
       result = i;
       break;
